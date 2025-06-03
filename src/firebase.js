e3bb5edf4c9dea71e41db6d97b2f2662f42e6c22
danielmul33 <danielescobarj33@gmail.com>
2025-06-03 09:13:40 -0500

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Reemplaza esto con la configuración de TU proyecto Firebase
+// Encuéntrala en tu Consola de Firebase:
+// Configuración del proyecto (icono de engranaje) > Tus apps > Configuración (abajo)
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_AUTH_DOMAIN",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializar Firestore y exportarlo para usarlo en otros archivos
+export const db = getFirestore(app);
