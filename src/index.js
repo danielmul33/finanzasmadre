1ec21a09a4072b49ac81dd3252a2194b462ef87c
danielmul33 <danielescobarj33@gmail.com>
2025-06-03 07:31:01 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import FinanceDashboard from './FinanceDashboard';
 // Si usas TailwindCSS, necesitarías importar el archivo CSS principal aquí, ej:
 // import './index.css'; // Asumiendo que tienes un archivo index.css con las directivas de Tailwind
