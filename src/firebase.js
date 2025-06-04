b54fe4e0071fe786b5e81ebf84ab689034da946f
danielmul33 <danielescobarj33@gmail.com>
2025-06-04 04:20:36 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,20 +2,21 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Reemplaza esto con la configuración de TU proyecto Firebase
-// Encuéntrala en tu Consola de Firebase:
-// Configuración del proyecto (icono de engranaje) > Tus apps > Configuración (abajo)
+// CONFIGURACIÓN CORRECTA Y COMPLETA DE TU PROYECTO FIREBASE
+// (Basada en tu última captura de pantalla de "Configuración del SDK")
 const firebaseConfig = {
   apiKey: "AIzaSyAff-MNOtgKk6gG_WqdxzPnI3MtceVVc",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "finanzasmul-8d731D",
-  storageBucket: "TU_STORAGE_BUCKET",
+  authDomain: "finanzasmul-8d731.firebaseapp.com",
+  projectId: "finanzasmul-8d731",
+  storageBucket: "finanzasmul-8d731.appspot.com",
   messagingSenderId: "31143773893",
-  appId: "TU_APP_ID"
+  appId: "1:31143773893:web:6e594d5e967b72611162ac",
+  measurementId: "G-PM594WT6B0" // Este también estaba en tu captura, es bueno incluirlo
 };
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
 // Inicializar Firestore y exportarlo para usarlo en otros archivos
-export const db = getFirestore(app, 'mis-finanzas-db');
+// (Conectándose a la base de datos por defecto de tu proyecto)
+export const db = getFirestore(app);
